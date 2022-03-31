62da53c6a706e86bc2ee9e77f39c1567f0d2cb61
Maksim Ivlev <brainlucker@gmail.com>
2022-03-31 21:58:30 +0300

    Getting random city from csv added

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,12 +1,19 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
 import lombok.Value;
 import lombok.val;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
 
@@ -18,9 +25,18 @@ public class DataGenerator {
         return LocalDate.now().plusDays(shift).format(formatter);
     }
 
+    @SneakyThrows
     public static String generateCity(String locale) {
-        val faker = new Faker(new Locale(locale));
-        return faker.address().cityName();
+        List<String> cities = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("./src/test/resources/cities.csv", StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                cities.add(line);
+            }
+        }
+        Random random = new Random();
+        int i = random.nextInt(cities.size() + 1);
+        return cities.get(i);
     }
 
     public static String generateName(String locale) {
